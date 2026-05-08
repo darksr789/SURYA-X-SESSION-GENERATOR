79c70c98b42cf434c37186139baf7e5019321291
darksr789 <dyt27380@gmail.com>
2026-05-08 19:15:36 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,131 +1,107 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const { exec } = require("child_process");
-let router = express.Router();
+let router = express.Router()
 const pino = require("pino");
 const {
-  default: makeWASocket,
-  useMultiFileAuthState,
-  delay,
-  makeCacheableSignalKeyStore,
-  Browsers,
-  jidNormalizedUser,
+    default: makeWASocket,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers,
+    jidNormalizedUser
 } = require("@whiskeysockets/baileys");
-const { upload } = require("./mega");
+const { upload } = require('./mega');
 
 function removeFile(FilePath) {
-  if (!fs.existsSync(FilePath)) return false;
-  fs.rmSync(FilePath, { recursive: true, force: true });
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
 }
 
-router.get("/", async (req, res) => {
-  let num = req.query.number;
-  async function SuryaPair() {
-    const { state, saveCreds } = await useMultiFileAuthState(`./session`);
-    try {
-      let SuryaPairWeb = makeWASocket({
-        auth: {
-          creds: state.creds,
-          keys: makeCacheableSignalKeyStore(
-            state.keys,
-            pino({ level: "fatal" }).child({ level: "fatal" })
-          ),
-        },
-        printQRInTerminal: false,
-        logger: pino({ level: "fatal" }).child({ level: "fatal" }),
-        browser: Browsers.macOS("Safari"),
-      });
+router.get('/', async (req, res) => {
+    let num = req.query.number;
+    async function DanuwaPair() {
+        const { state, saveCreds } = await useMultiFileAuthState(`./session`);
+        try {
+            let DanuwaPairWeb = makeWASocket({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: Browsers.macOS("Safari"),
+            });
 
-      if (!SuryaPairWeb.authState.creds.registered) {
-        await delay(1500);
-        num = num.replace(/[^0-9]/g, "");
-        const code = await SuryaPairWeb.requestPairingCode(num);
-        if (!res.headersSent) {
-          await res.send({ code });
-        }
-      }
-
-      SuryaPairWeb.ev.on("creds.update", saveCreds);
-      SuryaPairWeb.ev.on("connection.update", async (s) => {
-        const { connection, lastDisconnect } = s;
-        if (connection === "open") {
-          try {
-            await delay(10000);
-            const sessionSurya = fs.readFileSync("./session/creds.json");
-
-            const auth_path = "./session/";
-            const user_jid = jidNormalizedUser(SuryaPairWeb.user.id);
-
-            function randomMegaId(length = 6, numberLength = 4) {
-              const characters =
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-              let result = "";
-              for (let i = 0; i < length; i++) {
-                result += characters.charAt(
-                  Math.floor(Math.random() * characters.length)
-                );
-              }
-              const number = Math.floor(
-                Math.random() * Math.pow(10, numberLength)
-              );
-              return `${result}${number}`;
+            if (!DanuwaPairWeb.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await DanuwaPairWeb.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
             }
 
-            const mega_url = await upload(
-              fs.createReadStream(auth_path + "creds.json"),
-              `${randomMegaId()}.json`
-            );
+            DanuwaPairWeb.ev.on('creds.update', saveCreds);
+            DanuwaPairWeb.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection === "open") {
+                    try {
+                        await delay(10000);
+                        const sessionDanuwa = fs.readFileSync('./session/creds.json');
 
-            const string_session = mega_url.replace(
-              "https://mega.nz/file/",
-              ""
-            );
+                        const auth_path = './session/';
+                        const user_jid = jidNormalizedUser(DanuwaPairWeb.user.id);
 
-            const sid = `*SURYA-XD [The powerful WA BOT]*\n\n👉 ${string_session} 👈\n\n*This is the your Session ID, copy this id and paste into config.js file*\n\n*You can ask any question using this link*\n\n*wa.me/message/*994406616480*\n\n*You can join my whatsapp group*\n\n**`;
-            const mg = `🛑 *Do not share this code to anyone* 🛑`;
-            const dt = await SuryaPairWeb.sendMessage(user_jid, {
-              image: {
-                url: "https://files.catbox.moe/dp8vs9.jpg",
-              },
-              caption: sid,
+                      function randomMegaId(length = 6, numberLength = 4) {
+                      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                      let result = '';
+                      for (let i = 0; i < length; i++) {
+                      result += characters.charAt(Math.floor(Math.random() * characters.length));
+                        }
+                       const number = Math.floor(Math.random() * Math.pow(10, numberLength));
+                        return `${result}${number}`;
+                        }
+
+                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
+
+                        const string_session = mega_url.replace('https://mega.nz/file/', '');
+
+                        const sid = string_session;
+
+                        const dt = await DanuwaPairWeb.sendMessage(user_jid, {
+                            text: sid
+                        });
+
+                    } catch (e) {
+                        exec('pm2 restart danuwa');
+                    }
+
+                    await delay(100);
+                    return await removeFile('./session');
+                    process.exit(0);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode !== 401) {
+                    await delay(10000);
+                    DanuwaPair();
+                }
             });
-            const msg = await SuryaPairWeb.sendMessage(user_jid, {
-              text: string_session,
-            });
-            const msg1 = await SuryaPairWeb.sendMessage(user_jid, { text: mg });
-          } catch (e) {
-            exec("pm2 restart SURYA");
-          }
-
-          await delay(100);
-          return await removeFile("./session");
-          process.exit(0);
-        } else if (
-          connection === "close" &&
-          lastDisconnect &&
-          lastDisconnect.error &&
-          lastDisconnect.error.output.statusCode !== 401
-        ) {
-          await delay(10000);
-          SuryaPair();
+        } catch (err) {
+            exec('pm2 restart danuwa-md');
+            console.log("service restarted");
+            DanuwaPair();
+            await removeFile('./session');
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
         }
-      });
-    } catch (err) {
-      exec("pm2 restart SURYA-XD");
-      console.log("service restarted");
-      SuryaPair();
-      await removeFile("./session");
-      if (!res.headersSent) {
-        await res.send({ code: "Service Unavailable" });
-      }
     }
-  }
-  return await SuryaPair();
+    return await DanuwaPair();
 });
 
-process.on("uncaughtException", function (err) {
-  console.log("Caught exception: " + err);
-  exec("pm2 restart Surya");
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + err);
+    exec('pm2 restart danuwa');
 });
+
 
 module.exports = router;
