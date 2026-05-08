f236b393a3efaae897ea6b403e101d37a75a9bad
darksr789 <dyt27380@gmail.com>
2026-05-08 19:08:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-__path = process.cwd();
+__path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require("./pair");
-require("events").EventEmitter.defaultMaxListeners = 500;
-app.use("/code", code);
+let code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/code', code);
 
-app.use("/", async (req, res, next) => {
-  res.sendFile(__path + "/pair.html");
-});
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
-});
+    console.log(`⏩ Server running on http://localhost:` + PORT)
+})
 
-module.exports = app;
+module.exports = app
